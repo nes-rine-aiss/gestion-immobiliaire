fe2f2feb84271f7298bc6a035b0dfcef1de228d5
Zakaria <zakaria.braham@outlook.com>
2025-07-28 23:06:40 +0100

    Revert "add test route"
    This reverts commit dfd9471366b82be2df785d3382ad323e45714a98.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/test', function () {
-    return view('test');
-})->name('test');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
